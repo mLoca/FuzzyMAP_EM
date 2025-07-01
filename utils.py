bd072ce3ac5bc5657b6b17e75a0f0bc1bed2d2cd
mLoca <m.loca45@gmail.com>
2025-07-01 09:34:20 +0200

    json configuration experiment and util file

---
+++ utils.py
@@ -0,0 +1,83 @@
+import json
+
+import numpy as np
+
+from pomdp_example import State
+
+
+def _load_pomdp_config(config_path):
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+    return config
+
+
+def get_experiment_config(n_states=None):
+    config = _load_pomdp_config('experiment_config.json')
+    return_obj = {
+        "n_states": config["n_states"] if n_states is None else n_states,
+        "n_actions": config["n_actions"],
+        "obs_dim": config["obs_dim"],
+        "use_fuzzy": config["use_fuzzy"],
+        "rho_T": config["rho_T"],
+        "rho_O": config["rho_O"],
+        "transitions": config["transitions"],
+        "observations": config["observations"]
+    }
+    return return_obj
+
+
+def from_matrix_to_triplelist(transition_matrix):
+    states = ["healthy", "sick", "critical"]
+    actions = ["treat", "wait"]
+    triples = {}
+    for i, state in enumerate(states):
+        for j, action in enumerate(actions):
+            for k, next_state in enumerate(states):
+                if transition_matrix[i][j][k] > 0:
+                    triples.update({
+                        (state, action, next_state): transition_matrix[i][j][k]
+                    })
+    return triples
+
+
+def from_observation_config_to_observation(observation_config):
+    states = ["healthy", "sick", "critical"]
+    obs = {}
+    for i, state in enumerate(states):
+        #beta are the first and third and alha the second and fourth
+        obs.update({
+            state: (observation_config[i, 0], observation_config[i, 1],
+                    observation_config[i, 2], observation_config[i, 3])
+        })
+    return obs
+
+
+def from_beta_to_multivariate_normal(beta):
+    """
+    Convert beta parameters to a multivariate normal distribution.
+    :param beta: A list or array of beta parameters.
+    :return: A dictionary representing the multivariate normal distribution.
+    """
+    means = np.zeros((len(beta), len(list(beta.values())[0]) // 2))
+    covariances = np.zeros((len(beta),
+                            len(list(beta.values())[0]) // 2,
+                            len(list(beta.values())[0]) // 2))
+
+    count = 0
+    for state, val in beta.items():
+        a_t, b_t, a_s, b_s = beta[state]
+        # Beta mean and variance
+        mean_t = a_t / (a_t + b_t)
+        mean_s = a_s / (a_s + b_s)
+        var_t = (a_t * b_t) / ((a_t + b_t) ** 2 * (a_t + b_t + 1))
+        var_s = (a_s * b_s) / ((a_s + b_s) ** 2 * (a_s + b_s + 1))
+
+        means[count, :] = [mean_t, mean_s]
+        covariances[count, :, :] = [[var_t, 0],
+                                    [0, var_s]]
+        count += 1
+
+    return {
+        "means": means,
+        "covariances": covariances
+    }
