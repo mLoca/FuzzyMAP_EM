62a0509e59fdda8e01e6d5090974a58ced06a88d
m.loca45@gmail.com <m.loca45@gmail.com>
2025-07-01 22:31:23 +0200

    Code that will run all the experiment defined by the json file.

---
+++ experiments.py
@@ -0,0 +1,159 @@
+"""
+For each setup of the JSON fill, we will run three different experiments:
+ - fEM with fixed transition (few data and noisy observations)
+ - fEM with fixed observation (few data and noisy observations)
+ - fEM with 0 clue (few data and noisy observations)
+"""
+from matplotlib import pyplot as plt
+
+import utils
+from fuzzy_EM import FuzzyPOMDP, evaluate_fuzzy_reward_prediction, \
+    visualize_observation_distributions
+from fuzzy_model import build_fuzzymodel
+from fuzzy_model import create_continuous_medical_pomdp
+from fuzzy_model import collect_data
+
+from continouos_pomdp_example import STATES, generate_pomdp_data
+
+SEED = 15
+N_STATES = 3  # healthy, sick, critical
+N_ACTION = 2  # wait, treat
+OBS_DIM = 2  # test_result and symptoms (continuous)
+DATA_CONFIG = ["NORMAL", "NOISY", "FEW_DATA"]
+PARAMETERS_CONFIG = ["FIXED_TRANSITIONS", "FIXED_OBSERVATIONS", "ZERO_CLUE"]
+
+
+def run_experiment(trajector_lenght=5, n_trajectories=5, noise_sd=0.05, config=None, transition_matrices=None,
+                   observation_parameters=None):
+    """
+    Run the few data experiment with fixed transition and observation models.
+    """
+
+    # Create the POMDP environment
+    formatted_transition_matrices = None
+    formatted_observation_parameters = None
+    if transition_matrices is not None:
+        formatted_transition_matrices = utils.from_matrix_to_triplelist(transition_matrices)
+    if observation_parameters is not None:
+        formatted_observation_parameters = utils.from_observation_config_to_observation(observation_parameters)
+
+    pomdp_with_param = create_continuous_medical_pomdp(trans_params=formatted_transition_matrices,
+                                                       obs_params=formatted_observation_parameters)
+
+    # Collect data with few trials and horizon
+
+    original_pomdp, observations, actions, true_states, rewards = generate_pomdp_data(
+        trajectory_length=trajector_lenght, n_trajectories=n_trajectories, seed=SEED, noise_sd=noise_sd,
+        pomdp=pomdp_with_param
+    )
+
+    fuzzy_model = build_fuzzymodel(original_pomdp)
+
+    fit_and_performance(
+        original_pomdp, observations, actions, config,
+        fuzzy_model=fuzzy_model,
+        fix_transitions=transition_matrices,
+        fix_observations=observation_parameters)
+
+    fit_and_performance(
+        original_pomdp, observations, actions, config,
+        fuzzy_model=None,
+        fix_transitions=transition_matrices,
+        fix_observations=observation_parameters)
+
+
+def fit_and_performance(original_pomdp, observations, actions, config, fuzzy_model=None,
+                        fix_transitions=None, fix_observations=None):
+    """
+    Fit the fuzzy POMDP and evaluate its performance.
+    """
+    if fuzzy_model is not None:
+        use_fuzzy = True
+        evaluate_fuzzy_reward_prediction(300, 10, fuzzy_model=fuzzy_model, pomdp=original_pomdp)
+        rho_T = config.get("rho_T", 0.1)
+        rho_O = config.get("rho_O", 0.05)
+    else:
+        rho_T = 0.0
+        rho_O = 0.0
+        use_fuzzy = False
+
+    fuzzy_pomdp = FuzzyPOMDP(
+        n_states=N_STATES,
+        n_actions=N_ACTION,
+        obs_dim=OBS_DIM,
+        use_fuzzy=use_fuzzy,
+        fuzzy_model=fuzzy_model,
+        rho_T=rho_T,
+        rho_O=rho_O,
+        verbose=False,
+        fix_transitions=fix_transitions,
+        fix_observations=fix_observations
+    )
+
+    fuzzy_ll = fuzzy_pomdp.fit(
+        observations, actions,
+        max_iterations=200,
+        tolerance=1e-8
+    )
+
+    print(f"Fuzzy POMDP transitions:\n{fuzzy_pomdp.transitions}")
+
+    visualize_observation_distributions(fuzzy_pomdp, N_STATES, title_prefix="Fuzzy")
+    plt.show()
+
+    fuzzy_pomdp.compare_state_transitions(
+        fuzzy_pomdp.transitions,
+        original_pomdp.env.transition_model.transitions,
+        original_pomdp.agent.observation_model,
+        STATES
+    )
+
+
+def run_set_of_experiments():
+    """
+    Run a set of experiments with different configurations.
+    """
+    config = utils.get_experiment_config()
+
+    for key, value in config.items():
+        print(f"{key}")
+        for data_config in DATA_CONFIG:
+            traj_lenght = 5
+            n_trajectories = 20
+            noise_sd = 0.0
+
+            if data_config == "NORMAL":
+                traj_lenght = 5
+                n_trajectories = 20
+                noise_sd = 0.02
+            elif data_config == "NOISY":
+                traj_lenght = 5
+                n_trajectories = 15
+                noise_sd = 0.25
+            elif data_config == "FEW_DATA":
+                traj_lenght = 3
+                n_trajectories = 5
+                noise_sd = 0.02
+
+            for parameters_config in PARAMETERS_CONFIG:
+                print(f"Running experiment with {data_config} and {parameters_config}")
+                transition_matrices = None
+                observation_parameters = None
+
+                if parameters_config == "FIXED_TRANSITIONS":
+                    transition_matrices = value["transitions"]
+                elif parameters_config == "FIXED_OBSERVATIONS":
+                    observation_parameters = value["observations"]
+                elif parameters_config == "ZERO_CLUE":
+                    transition_matrices = value["transitions"]
+                    observation_parameters = value["observations"]
+
+                run_experiment(trajector_lenght=traj_lenght, n_trajectories=n_trajectories,
+                               noise_sd=noise_sd,
+                               config=config,
+                               transition_matrices=transition_matrices,
+                               observation_parameters=observation_parameters)
+
+
+if __name__ == "__main__":
+    run_set_of_experiments()
