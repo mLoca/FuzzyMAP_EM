8a22302fce46e0ab87b5e2f1651b928cc229d8a6
mLoca <m.loca45@gmail.com>
2025-12-19 13:42:42 +0100

    Refactor metrics module to replace Gaussian Mixture Models with multivariate normal distributions; update KL divergence computation and state matching functions to support new distribution type

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import linear_sum_assignment
+from scipy.stats import multivariate_normal
 
 
 def _from_beta_to_multivariate_normal(obs_model_real, state_name):
@@ -9,7 +10,6 @@ def _from_beta_to_multivariate_normal(obs_model_real, state_name):
     :param state_name: The name of the state for which to convert the parameters.
     :return: A GaussianMixture object representing the multivariate normal distribution.
     """
-    from sklearn.mixture import GaussianMixture
 
     means = []
     variances = []
@@ -19,32 +19,30 @@ def _from_beta_to_multivariate_normal(obs_model_real, state_name):
         means.append(a / (a + b))
         variances.append((a * b) / ((a + b) ** 2 * (a + b + 1)))
 
-    real_GMM = GaussianMixture(n_components=1, means_init=means)
-    real_GMM.means_ = np.array([means])
-    real_GMM.weights_ = [1]  # Assuming equal weights for simplicity
-    real_GMM.covariances_ = [np.diag(variances), np.diag(variances)]
+    cov_MVN = np.diag(variances)
+    real_MVN = multivariate_normal(mean=means, cov=cov_MVN)
 
-    return real_GMM
+    return real_MVN
 
 
-def _compute_KL_divergence(gmm_p, gmm_q):
+def _compute_KL_divergence(mvn_p, mvn_q):
     """
     Compute the KL divergence between two Gaussian Mixture Models.
-    :param gmm_p: The first Gaussian Mixture Model (GMM).
-    :param gmm_q: The second Gaussian Mixture Model (GMM).
+    :param mvn_p: The first Gaussian Mixture Model (GMM).
+    :param mvn_q: The second Gaussian Mixture Model (GMM).
     :return: Closed-form KL divergence D_KL(P \| Q) for single-component Gaussian GMMs.
     """
-    k = gmm_p.means_.shape[1]
+    k = mvn_p.mean.shape[0]
 
-    cov0 = gmm_p.covariances_[0]
-    cov1 = gmm_q.covariances_[0]
+    cov0 = mvn_p.cov
+    cov1 = mvn_q.cov
     cov0 += 1e-6 * np.eye(cov0.shape[0])
     cov1 += 1e-6 * np.eye(cov1.shape[0])
 
     cov1_inv = np.linalg.inv(cov1)
 
     trace_term = np.trace(cov1_inv @ cov0)
-    diff = gmm_q.means_ - gmm_p.means_
+    diff = mvn_q.mean - mvn_p.mean
     term = diff @ cov1_inv @ diff.T
 
     (sign0, logdet0) = np.linalg.slogdet(cov0)
@@ -54,21 +52,18 @@ def _compute_KL_divergence(gmm_p, gmm_q):
     return kl_approx
 
 
-def _from_mvn_to_gmm(mvns, state):
+def _mvn_definition_from_POMDP(mvns, state):
     """
-    Convert a multivariate normal distribution to a Gaussian Mixture Model.
+    Format the MVN using the scipy stats lib.
     :param mvns: A set of MVN representing the multivariate normal distribution in pomdp.
     :param state: The  state to convert.
-    :return: A GaussianMixture object.
+    :return: A MVN object.
     """
-    from sklearn.mixture import GaussianMixture
+    means = mvns.obs_means[state]
+    covariances = mvns.obs_covs[state]
 
-    gmm = GaussianMixture(n_components=1, means_init=mvns.obs_means[state])
-    gmm.means_ = mvns.obs_means[state].reshape(1, -1)
-    gmm.weights_ = [1]  # Assuming equal weights for simplicity
-    gmm.covariances_ = [mvns.obs_covs[state], mvns.obs_covs[state]]
-
-    return gmm
+    mvn = multivariate_normal(mean=means, cov=covariances)
+    return mvn
 
 
 def _from_mean_cov_to_gmm(means, cov):
@@ -78,14 +73,25 @@ def _from_mean_cov_to_gmm(means, cov):
     :param state: The  state to convert.
     :return: A GaussianMixture object.
     """
-    from sklearn.mixture import GaussianMixture
 
-    gmm = GaussianMixture(n_components=1, means_init=means)
-    gmm.means_ = means.reshape(1, -1)
-    gmm.weights_ = [1]  # Assuming equal weights for simplicity
-    gmm.covariances_ = [cov, cov]
+    return multivariate_normal(mean=means, cov=cov)
 
-    return gmm
+
+def _from_dist_to_mvn(params, dist_type, state_name=None):
+    """
+    Convert distribution parameters to a multivariate normal distribution.
+    :param params: The distribution parameters.
+    :param dist_type: Type of distribution ("beta" or "mvn").
+    :return: A multivariate normal distribution.
+    """
+    if dist_type == "mvn":
+        mean = params[0][state_name]["mean"]
+        cov = params[0][state_name]["cov"]
+        return _from_mean_cov_to_gmm(mean, cov)
+    elif dist_type == "beta":
+        return _from_beta_to_multivariate_normal(params, state_name)
+    else:
+        raise ValueError(f"Unsupported distribution type: {dist_type}")
 
 
 def normalize_cost_matrix(cost_matrix):
@@ -93,79 +99,88 @@ def normalize_cost_matrix(cost_matrix):
     return (cost_matrix - cost_matrix.min()) / (cost_matrix.max() - cost_matrix.min())
 
 
-def match_state_hungarian(learned_model, true_transitions, true_observations, states, alpha=0.35):
-        """
-        Match the states of the learned model to the true model using the Hungarian algorithm.
-        :param learned_model: The learned model with transition and observation parameters
-        :param true_transitions: The true transition parameters.
-        :param true_observations: The true observation parameters.
-        :param alpha: Weighting factor between transition and observation KL divergences.
-        :param states: List of state names.
-        :return: A list of matched state indices.
-        """
-        n_states = learned_model.n_states
+def match_state_hungarian(learned_model, true_transitions, true_observations, states, dist_type="beta", alpha=0.35):
+    """
+    Match the states of the learned model to the true model using the Hungarian algorithm.
+    :param learned_model: The learned model with transition and observation parameters
+    :param true_transitions: The true transition parameters.
+    :param true_observations: The true observation parameters.
+    :param alpha: Weighting factor between transition and observation KL divergences.
+    :param dist_type: Type of distribution for observations ("beta" or "mvn").
+    :param states: List of state names.
+    :return: A list of matched state indices.
+    """
+    n_states = learned_model.n_states
 
-        cost_trans = np.zeros((n_states, n_states))
-        cost_obs = np.zeros((n_states, n_states))
-        for row in range(n_states):
-            for col in range(n_states):
-                # Observation KL divergence: learned (row) vs true (col)
-                learned_obs_gmm = _from_mvn_to_gmm(learned_model, state=row)
-                true_obs_gmm = _from_beta_to_multivariate_normal(true_observations, state_name=states[col])
+    cost_trans = np.zeros((n_states, n_states))
+    cost_obs = np.zeros((n_states, n_states))
+    for row in range(n_states):
+        for col in range(n_states):
+            # Observation KL divergence: learned (row) vs true (col)
+            learned_obs_mvn = _mvn_definition_from_POMDP(learned_model, state=row)
+            true_obs_mvn = _from_dist_to_mvn(true_observations, dist_type, state_name=states[col])
 
-                KL_row_col = _compute_KL_divergence(learned_obs_gmm, true_obs_gmm)
-                KL_col_row = _compute_KL_divergence(true_obs_gmm, learned_obs_gmm)
-                cost_obs[row, col] = 0.5 * (KL_row_col + KL_col_row)
+            KL_row_col = _compute_KL_divergence(learned_obs_mvn, true_obs_mvn)
+            KL_col_row = _compute_KL_divergence(true_obs_mvn, learned_obs_mvn)
+            cost_obs[row, col] = 0.5 * (KL_row_col + KL_col_row)
 
-                # Transition L1 distance: learned (row) vs true (col)
-                cost_trans[row, col] = np.sum(np.abs(learned_model.transitions[row] - true_transitions[col]))
+            # Transition L1 distance: learned (row) vs true (col)
+            cost_trans[row, col] = np.sum(np.abs(learned_model.transitions[row] - true_transitions[col]))
 
-        cost_trans = normalize_cost_matrix(cost_trans)
-        cost_obs = normalize_cost_matrix(cost_obs)
+    cost_trans = normalize_cost_matrix(cost_trans)
+    cost_obs = normalize_cost_matrix(cost_obs)
 
-        total_cost = alpha * cost_trans + (1 - alpha) * cost_obs
-        row_ind, col_ind = linear_sum_assignment(total_cost)
+    total_cost = alpha * cost_trans + (1 - alpha) * cost_obs
+    row_ind, col_ind = linear_sum_assignment(total_cost)
 
-        # permutation_order[true_index] = learned_index
-        permutation_order = np.zeros(n_states, dtype=int)
-        permutation_order[col_ind] = row_ind
+    # permutation_order[true_index] = learned_index
+    permutation_order = np.zeros(n_states, dtype=int)
+    permutation_order[col_ind] = row_ind
 
-        # perm_map_dict[true_index] = learned_index
-        perm_map_dict = {int(c): int(r) for r, c in zip(row_ind, col_ind)}
+    # perm_map_dict[true_index] = learned_index
+    perm_map_dict = {int(c): int(r) for r, c in zip(row_ind, col_ind)}
 
-        return permutation_order, perm_map_dict
+    return permutation_order, perm_map_dict
 
 
-def compute_error_metrics(learned_model, true_transitions, true_observations, states):
-
+def compute_error_metrics(learned_model, true_transitions, true_observations, states, dist_type="beta"):
     """
     Compute error metrics between the learned model and the true model.
     :param learned_model: The learned model with transition and observation parameters
     :param true_transitions: The true transition parameters.
     :param true_observations: The true observation parameters.
     :param states: List of state names.
+    :param dist_type: Type of distribution for observations ("beta" or "mvn").
     :return: A dictionary containing final KL divergences, average L1 error, and permutation map.
     """
     n_states = learned_model.n_states
     n_actions = learned_model.n_actions
 
-    perm_ord, perm_map = match_state_hungarian(learned_model, true_transitions, true_observations, states, alpha=0.1)
+    perm_ord, perm_map = match_state_hungarian(learned_model, true_transitions, true_observations, states,
+                                               dist_type=dist_type, alpha=0.1)
     aligned_means = learned_model.obs_means[perm_ord]
     aligned_covs = learned_model.obs_covs[perm_ord]
 
     # Final KL
     final_kl = np.zeros(n_states)
     for state in range(n_states):
-        learned_obs_gmm = _from_mean_cov_to_gmm(aligned_means[state], aligned_covs[state])
-        true_obs_gmm = _from_beta_to_multivariate_normal(true_observations, state_name=states[state])
+        learned_obs_mvn = _from_mean_cov_to_gmm(aligned_means[state], aligned_covs[state])
+        true_obs_mvn = _from_dist_to_mvn(true_observations, dist_type, state_name=states[state])
 
-        KL_row_col = _compute_KL_divergence(true_obs_gmm, learned_obs_gmm)
-        KL_col_row = _compute_KL_divergence(learned_obs_gmm, true_obs_gmm)
+        KL_row_col = _compute_KL_divergence(true_obs_mvn, learned_obs_mvn)
+        KL_col_row = _compute_KL_divergence(learned_obs_mvn, true_obs_mvn)
         final_kl[state] = 0.5 * (KL_row_col + KL_col_row)
 
     aligned_transitions = learned_model.transitions[perm_ord][:, :, perm_ord]
     l1_diff = np.sum(np.abs(aligned_transitions - true_transitions))
     avg_l1_error = l1_diff / (n_states * n_actions)
+
+    #print the true mean and cov
+    #TODO: remove this print statement later
+    #print("True Means and Covariances:")
+    #for state in range(n_states):
+    #    true_obs_mvn = _from_dist_to_mvn(true_observations, dist_type, state_name=states[state])
+    #    print(f"State {state}: Mean = {true_obs_mvn.mean}, Covariance = {true_obs_mvn.cov}")
 
     return {
         "final_kl": final_kl,
